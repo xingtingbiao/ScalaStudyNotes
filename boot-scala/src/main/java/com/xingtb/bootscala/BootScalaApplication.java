c63eca8fb8c7dc72487d6be37a6cc73691bdd301
xingtingbiao <18551617816@163.com>
2018-09-24 15:52:36 +0800

    feature: 实战项目

---
+++ boot-scala/src/main/java/com/xingtb/bootscala/BootScalaApplication.java
@@ -0,0 +1,12 @@
+package com.xingtb.bootscala;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootScalaApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(BootScalaApplication.class, args);
+  }
+}
---
+++ boot-scala/src/test/java/com/xingtb/bootscala/BootScalaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.xingtb.bootscala;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BootScalaApplicationTests {
+
+  @Test
+  public void contextLoads() {
+  }
+
+}
